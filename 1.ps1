39ac381a27a10cef48ac2685b32058a47fcf6317
Hiram Xu <hxm14375@outlook.com>
2017-08-20 12:34:09 +0800

    12

---
+++ 1.ps1
@@ -0,0 +1,7 @@
+&nasm IPL.txt -o IPL.bin
+&nasm ./BasicIOModule/print.txt -o print.bin
+&nasm core.txt -o core.bin
+&nasm ./BasicIOModule/DiskIO.txt -o DiskIO.bin
+&nasm ./BasicIOModule/Mprint.txt -o Mprint.bin
+&nasm ./XFileSystem/loadFileSystem.txt -o LoadFileSystem.bin
+&nasm ./XFileSystem/list.txt -o list.bin
