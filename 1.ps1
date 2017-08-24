8e07c606969a7495c3858a095da1d1c2567d6209
FTLJ-I <hxm14375@outlook.com>
2017-08-24 16:14:29 +0800

    12

--- 1.ps1
+++ 1.ps1
@@ -2,6 +2,5 @@
 &nasm ./BasicIOModule/print.txt -o Bin/print.bin
 &nasm core.txt -o Bin/core.bin
 &nasm ./BasicIOModule/DiskIO.txt -o Bin/DiskIO.bin
-&nasm ./BasicIOModule/Mprint.txt -o Bin/Mprint.bin
 &nasm ./XFileSystem/loadFileSystem.txt -o Bin/LoadFileSystem.bin
 &nasm ./XFileSystem/list.txt -o Bin/list.bin
